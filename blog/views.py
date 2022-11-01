60c29d688ee642c5aa987ffca77dfb726eaca79f
Jaredslocker21 <jaredslocker21@gmail.com>
2022-11-01 14:51:11 +0000

    views if comment is valid

--- blog/views.py
+++ blog/views.py
@@ -27,11 +27,12 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForms()
             },
         )
-        
+
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -40,7 +41,16 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form = CommentForms(data=request.Post)
+        comment_form = CommentForms(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForms()
 
         return render(
             request,
@@ -48,6 +58,7 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": True,
                 "liked": liked,
                 "comment_form": CommentForms()
             },
